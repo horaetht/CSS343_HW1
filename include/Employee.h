727bce0a737d87597c3c898d04a97f6e2f9d36a3
horaetht <horace32162@gmail.com>
2026-04-19 17:13:24 -0700

    documentation for employee.h

--- include/Employee.h
+++ include/Employee.h
@@ -2,12 +2,12 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
-class Employee {
-    protected:
+class Employee { // Abstract base class
+    protected: // fields accessible to derived classes
         std::string name;
         int id;
 
-    public:
+    public: // public member functions
         Employee(std::string name, int id);
         virtual ~Employee();
         virtual double calculatePay() const = 0; // Pure virtual function
--- src/Employee.cpp
+++ src/Employee.cpp
@@ -6,10 +6,6 @@ Employee::Employee(string name, int id) : name(name), id(id) {} // Constructor t
 
 Employee::~Employee() {} // Base class destructor
 
-// double Employee::calculatePay() const {
-//     return 0.0; // Base class does not calculate pay
-// } // pure vitual function, no implementation needed
-
 void Employee::display() const { // Base class display function to show name and id
     cout << "Employee Name: " << name << endl;
     cout << "Employee ID: " << id << endl;
